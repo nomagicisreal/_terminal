986bee76452dc93f989a6efa86f5b46f2a5057b1
nomisal <tryeverythingican@gmail.com>
2025-05-12 14:21:43 +0800

    2025-05-12

---
+++ _go/main.go
@@ -0,0 +1,39 @@
+package main
+
+// "os/exec"
+
+// const name = "downloader"
+
+func main() {
+	// TODO: if there is no "downloader" executable file, run go build
+	// TODO: execute "downloader"
+	// wd, err := os.Getwd()
+	// if err != nil {
+	// 	fmt.Println(err.Error())
+	// }
+	// // fs.ReadDir()
+	// os.DirFS(wd)
+	// fmt.Println()
+
+	// cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+	// stdout, err := cmd.Output()
+
+	// if err != nil {
+	//     fmt.Println(err.Error())
+	//     return
+	// }
+
+	// // Print the output
+	// fmt.Println(string(stdout))
+
+	// scanner := bufio.NewScanner(os.Stdin)
+	// for scanner.Scan() {
+	// 	fmt.Println(scanner.Text())
+	// }
+
+	// if scanner.Err() != nil {
+	// 	// Handle error.
+	// }
+
+	// Tempt()
+}
---
+++ _go/scritp_ytdlp.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func Tempt() {
+	fmt.Println("hello")
+}
